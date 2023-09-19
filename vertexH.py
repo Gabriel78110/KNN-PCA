dd7b24dfe609707ccbc2047a983800ad6622badd
Gabriel Moryoussef <gabriel.moryoussef@gmail.com>
2023-09-18 20:13:08 -0400

    add different parameters t and N for tresholds and number of neighbors respectively

--- vertexH.py
+++ vertexH.py
@@ -3,20 +3,23 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import pdist, squareform
-from scipy.spatial import KDTree
+from scipy.spatial import KDTree,cKDTree
 from spa import SuccessiveProj
 
-N = 1000
+N_sample = 1000
 K = 3
+d = 2
+
 def runif_in_simplex(n, vertex, sigma=0.5):
-  ''' Return uniformly random vector in the n-simplex '''
+  ''' Return uniformly random vector in the n-simplex 
+  vertex: d * K array'''
 
   k = np.random.exponential(scale=1.0, size=n)
   w = k/sum(k)
-  return np.dot(vertex,np.transpose(w)) + np.random.normal(scale=sigma, size=K-1)
+  return np.dot(vertex,np.transpose(w)) + np.random.normal(scale=sigma, size=d)
 
 def get_sample(N_sample, vertex):
-    samples = np.array([runif_in_simplex(n=K,vertex=vertex) for _ in range(N_sample)]).reshape(-1,K-1)
+    samples = np.array([runif_in_simplex(n=K,vertex=vertex) for _ in range(N_sample)]).reshape(-1,d)
     return samples
 
 def plot_simplex(vertex,samples):
@@ -43,40 +46,40 @@ def knn(samples,M=20):
     eps = l_max/M
     return eps
 
-# def points_within_epsilon(points, epsilon,N=3):
-#     # Create a KD-Tree from the input points
-#     kdtree = KDTree(points)
-    
-#     # Initialize an empty list to store the results
-#     results = []
-    
-#     # Query the KD-Tree for points within epsilon for each point
-#     for j, point in enumerate(points):
-#         #print("epsilon=",epsilon)
-#         indices = kdtree.query_ball_point(point, epsilon)
-#         neighbors = [points[i] for i in indices if i!=j]
-#         if len(neighbors) >= N:
-#             results.append(np.mean(neighbors,axis=0))
-    
-#     return np.array(results).reshape(-1,K-1)
+
+def compute_points_within_epsilon(point_cloud, epsilon, N,t):
+    # Create a KD-tree for fast neighbor searches
+    kdtree = cKDTree(point_cloud)
+
+    # Query for all points within epsilon distance
+    neighbor_indices = kdtree.query_ball_tree(kdtree, r=epsilon)
+
+    # Initialize a list to store the filtered and replaced points
+    filtered_points = []
+
+    for i, point in enumerate(point_cloud):
+        # Exclude the point itself from the list of neighbors
+        neighbor_indices[i].remove(i)
+        
+        # If there are fewer than t neighbors, discard this point
+        if len(neighbor_indices[i]) < t:
+            continue
+
+        # If there are more than t neighbors, but fewer than N neighbors, 
+        # take the average of all neighbors.
+        if len(neighbor_indices[i]) <= N:
+            avg_neighbor = np.mean([point_cloud[j] for j in neighbor_indices[i]], axis=0)
+        else:
+            # Get the N closest neighbors
+            distances = kdtree.query(point, k=N+1)
+            avg_neighbor = np.mean([point_cloud[j] for j in distances[1]], axis=0)
+
+        # Append the average neighbor to the filtered list
+        filtered_points.append(avg_neighbor)
+
+    return np.array(filtered_points).reshape(-1,d)
 
 
-
-def compute_points_within_epsilon(point_cloud, epsilon, N=3):
-    kdtree = KDTree(point_cloud)
-    result = []
-
-    for point in point_cloud:
-        # Query the KD-Tree to find points within epsilon distance
-        indices_within_epsilon = kdtree.query_ball_point(point, epsilon)
-
-        if len(indices_within_epsilon) >= N:
-            # If more than N points within epsilon distance, keep only the closest N points
-            distances = kdtree.query(point, k=N+1)  # Include the point itself in the query
-            closest_indices = distances[1]
-            result.append(np.mean(point_cloud[closest_indices],axis=0))
-
-    return np.array(result).reshape(-1,K-1)
 
 if __name__ == '__main__':
 
@@ -84,21 +87,21 @@ if __name__ == '__main__':
 
     vertex=np.array([[1,2,5],[1,4,2]])
     #samples = get_sample(N_sample = N, vertex=vertex)
-    samples = np.load("samples_smallvar.npy")
+    samples = np.load("samples_bigvar.npy")
     #np.save('my_array.npy', samples)
     eps = knn(samples)
-    print(eps)
     #X_tilde = points_within_epsilon(samples,eps)
-    X_tilde = compute_points_within_epsilon(samples,eps)
-    vertices_knn = SuccessiveProj(X_tilde,K)
-    vertices_knn = np.concatenate((vertices_knn, vertices_knn[0].reshape(1,-1)),axis=0)
-    vertices_spa = SuccessiveProj(samples,K)
-    vertices_spa = np.concatenate((vertices_spa, vertices_spa[0].reshape(1,-1)),axis=0)
-    plot_simplex(vertex, samples)
-    plt.plot(vertices_knn[:,0], vertices_knn[:,1],"b--", marker='o',label="KNN-SPA reconstruction")
-    plt.plot(vertices_spa[:,0], vertices_spa[:,1],"g--", marker='o',label="SPA reconstruction")
-    plt.legend()
-    plt.title("Sigma = 0.5")
-    plt.show()
+    for t in range(1,20):
+        X_tilde = compute_points_within_epsilon(samples,eps,N=3,t=t)
+        vertices_knn = SuccessiveProj(X_tilde,K)
+        vertices_knn = np.concatenate((vertices_knn, vertices_knn[0].reshape(1,-1)),axis=0)
+        vertices_spa = SuccessiveProj(samples,K)
+        vertices_spa = np.concatenate((vertices_spa, vertices_spa[0].reshape(1,-1)),axis=0)
+        plot_simplex(vertex, samples)
+        plt.plot(vertices_knn[:,0], vertices_knn[:,1],"b--", marker='o',label="KNN-SPA reconstruction")
+        plt.plot(vertices_spa[:,0], vertices_spa[:,1],"g--", marker='o',label="SPA reconstruction")
+        plt.legend()
+        plt.title(f"t = {t}")
+        plt.show()
 
 
