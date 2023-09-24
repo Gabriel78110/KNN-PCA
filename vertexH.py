77ea865eecafdced1be14031d6c0affc3e02419a
Gabriel Moryoussef <gabriel.moryoussef@gmail.com>
2023-09-24 09:21:59 -0400

    split figure 1 in 2 subplots

--- vertexH.py
+++ vertexH.py
@@ -3,30 +3,39 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import pdist, squareform
+from scipy.spatial import distance
+from scipy.optimize import linear_sum_assignment
 from scipy.spatial import KDTree,cKDTree
 from spa import SuccessiveProj
+from utils import find_max_distance_to_average, find_min_distance_to_average
 
 N_sample = 1000
 K = 3
 d = 2
 
-def runif_in_simplex(n, vertex, sigma=0.5):
+def runif_in_simplex(n, vertex, sigma=0.5,d=2):
   ''' Return uniformly random vector in the n-simplex 
   vertex: d * K array'''
 
   k = np.random.exponential(scale=1.0, size=n)
   w = k/sum(k)
-  return np.dot(vertex,np.transpose(w)) + np.random.normal(scale=sigma, size=d)
+  #+ np.random.normal(scale=sigma, size=d)
+  return np.dot(vertex,np.transpose(w))
 
-def get_sample(N_sample, vertex):
-    samples = np.array([runif_in_simplex(n=K,vertex=vertex) for _ in range(N_sample)]).reshape(-1,d)
+def get_sample(N_sample, vertex,pure_nodes=10,sigma=0.5,d=2):
+    #+ np.random.normal(scale=sigma, size=(K*pure_nodes,d))
+    vertices_pure = np.tile(np.transpose(vertex),(pure_nodes,1))
+    samples = np.concatenate((vertices_pure,np.array([runif_in_simplex(n=K,vertex=vertex,d=d) for _ in range(N_sample-pure_nodes*K)]).reshape(-1,d)))
     return samples
 
-def plot_simplex(vertex,samples):
+def plot_simplex(vertex,samples,ax,plot_data=True):
     vertex_t = np.transpose(vertex)
     vertex_t = np.concatenate((vertex_t,vertex_t[0].reshape(1,-1)),axis=0)
-    plt.scatter(samples[:,0], samples[:,1], s=0.3, label="original sample")
-    plt.plot(vertex_t[:,0], vertex_t[:,1], "r--", marker='o', label="true vertices")
+    if plot_data:
+        ax.scatter(samples[:,0], samples[:,1], s=0.3, label="sample data",c="black")
+        ax.plot(vertex_t[:,0], vertex_t[:,1], "k--", marker='o', label="ideal simplex")
+    else:
+        ax.plot(vertex_t[:,0], vertex_t[:,1], "k--", marker='o', label="ideal simplex")
     #plt.show()
 
 
@@ -47,7 +56,9 @@ def knn(samples,M=20):
     return eps
 
 
-def compute_points_within_epsilon(point_cloud, epsilon, N,t):
+def compute_points_within_epsilon(point_cloud, epsilon, N,t,d=2):
+    """point_cloud: N_samples * d numpy array"""
+
     # Create a KD-tree for fast neighbor searches
     kdtree = cKDTree(point_cloud)
 
@@ -58,8 +69,7 @@ def compute_points_within_epsilon(point_cloud, epsilon, N,t):
     filtered_points = []
 
     for i, point in enumerate(point_cloud):
-        # Exclude the point itself from the list of neighbors
-        neighbor_indices[i].remove(i)
+        #neighbor_indices[i].remove(i)
         
         # If there are fewer than t neighbors, discard this point
         if len(neighbor_indices[i]) < t:
@@ -71,13 +81,30 @@ def compute_points_within_epsilon(point_cloud, epsilon, N,t):
             avg_neighbor = np.mean([point_cloud[j] for j in neighbor_indices[i]], axis=0)
         else:
             # Get the N closest neighbors
-            distances = kdtree.query(point, k=N+1)
+            distances = kdtree.query(point, k=N)
             avg_neighbor = np.mean([point_cloud[j] for j in distances[1]], axis=0)
 
         # Append the average neighbor to the filtered list
         filtered_points.append(avg_neighbor)
 
     return np.array(filtered_points).reshape(-1,d)
+
+# Compute the pairing of points with the minimum Euclidean distance
+def compute_min_distance_pairing(point_cloud1, point_cloud2,max=True):
+    # Calculate the pairwise Euclidean distances between all points in both clouds
+    distance_matrix = distance.cdist(point_cloud1, point_cloud2)
+    row_ind, col_ind = linear_sum_assignment(distance_matrix)
+
+
+    # Return the indices of paired points and the minimum total distance
+    paired_points_indices = list(zip(row_ind, col_ind))
+    if not max:
+        min_total_distance = distance_matrix[row_ind, col_ind].sum()
+
+        return paired_points_indices, min_total_distance
+
+    min_total_distance = np.max(distance_matrix[row_ind, col_ind])
+    return paired_points_indices, min_total_distance
 
 
 
@@ -91,17 +118,21 @@ if __name__ == '__main__':
     #np.save('my_array.npy', samples)
     eps = knn(samples)
     #X_tilde = points_within_epsilon(samples,eps)
-    for t in range(1,20):
-        X_tilde = compute_points_within_epsilon(samples,eps,N=3,t=t)
-        vertices_knn = SuccessiveProj(X_tilde,K)
-        vertices_knn = np.concatenate((vertices_knn, vertices_knn[0].reshape(1,-1)),axis=0)
-        vertices_spa = SuccessiveProj(samples,K)
-        vertices_spa = np.concatenate((vertices_spa, vertices_spa[0].reshape(1,-1)),axis=0)
-        plot_simplex(vertex, samples)
-        plt.plot(vertices_knn[:,0], vertices_knn[:,1],"b--", marker='o',label="KNN-SPA reconstruction")
-        plt.plot(vertices_spa[:,0], vertices_spa[:,1],"g--", marker='o',label="SPA reconstruction")
-        plt.legend()
-        plt.title(f"t = {t}")
-        plt.show()
+    X_tilde = compute_points_within_epsilon(samples,epsilon=0.1,N=4,t=3)
+    vertices_knn = SuccessiveProj(X_tilde,K)
+    vertices_knn = np.concatenate((vertices_knn, vertices_knn[0].reshape(1,-1)),axis=0)
+    vertices_spa = SuccessiveProj(samples,K)
+    vertices_spa = np.concatenate((vertices_spa, vertices_spa[0].reshape(1,-1)),axis=0)
 
 
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,3))
+    plot_simplex(vertex, samples,ax1)
+    ax1.plot(vertices_spa[:,0], vertices_spa[:,1],"g--", marker='o',label="SPA")
+
+    plot_simplex(vertex, samples,ax2,plot_data=False)
+    ax2.scatter(X_tilde[:,0], X_tilde[:,1], s=0.3, label="psuedo-points",c="b")
+    ax2.plot(vertices_knn[:,0], vertices_knn[:,1],"c--", marker='o',label="PPSPA (step 2)")
+    ax1.legend()
+    ax2.legend()
+    #plt.title(f"t = {t}")
+    plt.show()
